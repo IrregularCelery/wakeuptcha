cc1ec2758fc8591d33e2839aa6f73f076c45449a
Mohsen Rad <irregular.celery@gmail.com>
2025-02-19 18:15:39 +0330

    Rename `settings.gd` to `constants.gd`

---
+++ scripts/constants.gd
@@ -0,0 +1,8 @@
+extends Node
+
+# For getting correct scaling of the elements
+const DESIGN_WIDTH: int = 480
+const DESIGN_HEIGHT: int = 854
+
+# Padding for top and bottom of the container in `landscape`
+const PORTRAIT_PADDING_Y: float = 0.05 # 5 percent of viewport height (per side)
--- scripts/settings.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-const PORTRAIT_WIDTH  : int = 480
-const PORTRAIT_HEIGHT : int = 854
-const PORTRAIT_PADDING	  : int = 32
