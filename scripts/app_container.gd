ffe0ddfe94efd79115baa88fe7d467bfed9779d5
Mohsen Rad <irregular.celery@gmail.com>
2025-02-20 18:07:19 +0330

    Refactor `update_layout()` function in `app_container`

--- scripts/app_container.gd
+++ scripts/app_container.gd
@@ -17,30 +17,23 @@ func is_portrait() -> bool:
 func update_layout() -> void:
 	if is_portrait():
 		# Portrait layout: Content takes full screen, ignoring margins
+		container.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
 
-		container.set_anchors_preset(Control.PRESET_FULL_RECT)
-		container.offset_left = 0
-		container.offset_right = 0
-		container.offset_top = 0
-		container.offset_bottom = 0
-	else:
-		# Landscape layout: Portrait container in center
-		
-		# For having padding on top and bottom of the container
-		var multiplier: float = get_viewport_rect().size.y \
-		* (1.0 - Constants.PORTRAIT_PADDING_Y * 2.0) / Constants.DESIGN_HEIGHT
-		
-		var container_size := Vector2(
-			Constants.DESIGN_WIDTH,
-			Constants.DESIGN_HEIGHT
-		) * multiplier
+		return
 
-		container.custom_minimum_size = container_size
-		container.set_anchors_preset(Control.PRESET_CENTER)
-		container.offset_left = 0
-		container.offset_right = 0
-		container.offset_top = 0
-		container.offset_bottom = 0
+	# Landscape layout: Portrait container in center
+
+	# For having padding on top and bottom of the container
+	var multiplier: float = get_viewport_rect().size.y \
+	* (1.0 - Constants.PORTRAIT_PADDING_Y * 2.0) / Constants.DESIGN_HEIGHT
+
+	var container_size := Vector2(
+		Constants.DESIGN_WIDTH,
+		Constants.DESIGN_HEIGHT,
+	) * multiplier
+
+	container.custom_minimum_size = container_size
+	container.set_anchors_and_offsets_preset(Control.PRESET_CENTER)
 
 
 func _on_window_resize() -> void:
